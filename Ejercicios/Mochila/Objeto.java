fa152a89fad5fe88239b6237f1463b3a2dfbc58a
JuanGabrielArenasMeneses <122894273+JuanGabrielArenasMeneses@users.noreply.github.com>
2023-03-07 16:35:45 -0500

    Prueba de diferentes Collections

--- Ejercicios/Mochila/Objeto.java
+++ Ejercicios/Mochila/Objeto.java
@@ -3,4 +3,22 @@ package Ejercicios.Mochila;
 public class Objeto {
     private Integer peso;
     private Integer valor;
+
+    public Objeto(){}
+    public Objeto(int p,int v){
+        this.peso=p;
+        this.valor=v;
+    }
+    public void setPeso(int p) {
+        this.peso=p;
+    }
+    public void setValor(int v) {
+        this.valor=v;
+    }
+    public int getPeso() {
+        return this.peso;
+    }
+    public int getValor() {
+        return this.valor;
+    }
 }
--- Menu.java
+++ Menu.java
@@ -2,7 +2,7 @@ import Ejercicios.*;
 import Ejercicios.Mochila.Mochila;
 public class Menu {
     public static void main(String[] args) {
-
+        Mochila.crearObjetos();
     }
 
 
